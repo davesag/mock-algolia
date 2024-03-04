f1b194a68281af744f07aaebf1f13ebda3a1a8a3
Dave Sag <davesag@gmail.com>
2024-03-04 17:58:24 +1100

    n/a removed unmaintained chai-as-promised dependency

--- test/unitTestHelper.js
+++ test/unitTestHelper.js
@@ -1,8 +1,6 @@
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
-const chaiAsPromised = require('chai-as-promised')
 const chaiAlmost = require('chai-almost')
 
 chai.use(sinonChai)
-chai.use(chaiAsPromised)
 chai.use(chaiAlmost(10))
